d2b3a535d35fe024eb7215e740647455a5e1d492
Chet Jones <chetjones003@gmail.com>
2023-06-14 14:23:00 -0500

    finished

---
+++ 7kyu/basic_mathematical_operations/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn basic_op(operator: char, value1: i32, value2: i32) -> i32 {
+    match operator {
+        '+' => value1 + value2,
+        '-' => value1 - value2,
+        '*' => value1 * value2,
+        '/' => value1 / value2,
+        _ => panic!("No implementation found for operator {operator}"),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::basic_op;
+
+    fn dotest(op: char, v1: i32, v2: i32, expected: i32) {
+        let actual = basic_op(op, v1, v2);
+        assert!(actual == expected,
+            "With Operator = '{op}', value1 = {v1}, value2 = {v2}\nExpected = {expected}, but got {actual}");
+    }
+
+    #[test]
+    fn basic_tests() {
+        dotest('+', 4, 7, 11);
+        dotest('-', 15, 18, -3);
+        dotest('*', 5, 5, 25);
+        dotest('/', 49, 7, 7);
+    }
+}
